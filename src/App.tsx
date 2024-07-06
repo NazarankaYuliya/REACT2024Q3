5c4f4ce427ad22de84468f87a12caa95f729c8cf
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-07-06 13:22:52 +0200

    feat: add search logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,40 @@ import Search from './components/Search';
 import Results from './components/Results';
 import ErrorBoundary from './components/ErrorBoundary';
 import ErrorButton from './components/ErrorButton';
+import { Character } from './types/Character';
 
 class App extends Component {
+  state = {
+    searchResults: [],
+    isLoading: false,
+  };
+
+  updateResults = (results: Character[]) => {
+    this.setState({ searchResults: results, isLoading: false });
+  };
+
+  handleLoading = (isLoading: boolean) => {
+    this.setState({ isLoading });
+  };
+
+  handleReturnToSearch = () => {
+    this.setState({ searchResults: [], isLoading: false });
+  };
+
   render() {
     return (
       <>
         <ErrorBoundary>
           <ErrorButton />
           <Header />
-          <Search />
-          <Results />
+          <Search
+            updateResults={this.updateResults}
+            setLoading={this.handleLoading}
+          />
+          <Results
+            results={this.state.searchResults}
+            isLoading={this.state.isLoading}
+          />
         </ErrorBoundary>
       </>
     );
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,8 +1,37 @@
 import { Component } from 'react';
+import { Character } from '../types/Character';
+import CharacterCard from './CharacterCard';
 
-class Results extends Component {
+interface ResultsProps {
+  results: Character[];
+  isLoading: boolean;
+}
+
+class Results extends Component<ResultsProps> {
   render() {
-    return <div>Results</div>;
+    const { results, isLoading } = this.props;
+
+    if (isLoading) {
+      return <p className="loading">Loading...</p>;
+    }
+
+    if (results.length === 0) {
+      return (
+        <div className="results">
+          <p className="not-found">No characters found.</p>
+        </div>
+      );
+    }
+
+    return (
+      <div className="results">
+        {results.map((character) => (
+          <div key={character.id} className="card">
+            <CharacterCard character={character} />
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
